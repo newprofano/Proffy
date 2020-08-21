6f53694b38481df313016275020933c76e83f8c6
Christian Furlan <newera.cabal@hotmail.com>
2020-08-21 12:29:56 -0300

    release

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, ImageBackground, Text } from "react-native";
+import styles from "./styles";
+import giveClassesBgImage from "../../assets/images/give-classes-background.png";
+import { RectButton } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+
+function GiveClasses() {
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack() {
+    goBack();
+  }
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={giveClassesBgImage}
+        style={styles.content}
+        resizeMode="contain"
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor em nossa
+          plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Tudo Bem</Text>
+      </RectButton>
+    </View>
+  );
+}
+
+export default GiveClasses;
